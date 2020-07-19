132f5a5ef7218b840edb1292b679efaf740721e1
chihirotanaka <61111335+chihirotanaka@users.noreply.github.com>
2020-07-19 23:37:57 +0900

    [finish]4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,15 @@
+
+public class Chapter04 {
+
+	public static void main(String[] args) {
+
+		String str = "chihiro";
+		System.out.println(str);
+
+		str = "chihirochan";
+		final int LUCKY_NUMBER = 330;
+		System.out.println(LUCKY_NUMBER);
+		System.out.println(str);
+
+	}
+}
