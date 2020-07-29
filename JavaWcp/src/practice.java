3f5ee43e8b28c6686b92726aec7ec1a4ed1bb871
chihirotanaka <61111335+chihirotanaka@users.noreply.github.com>
2020-07-29 14:17:04 +0900

    [start]7

---
+++ JavaWcp/src/practice.java
@@ -0,0 +1,14 @@
+
+public class practice {
+	
+  boolean a = true;
+  boolean b = false;
+  boolean c ;
+  
+  c = a && true; //c = true
+  c = a && false; //c = false
+  c = b || true; //c = true
+  c = b || false; //c =false
+  c = !a;         //c =false
+  c = !b;        //c =true
+}
