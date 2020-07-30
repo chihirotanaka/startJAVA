d337e2389c8c7cdf8de74717b2af864f2dc33f6d
chihirotanaka <61111335+chihirotanaka@users.noreply.github.com>
2020-07-30 14:20:01 +0900

    [start]9

--- JavaWcp/src/Chapter08.java
+++ JavaWcp/src/Chapter08.java
@@ -5,5 +5,17 @@ public class Chapter08 {
 		while ( num < 5 );
 		num *= 2;
 		System.out.println("While =" + num );
+		
+		int num array = { 1, 2, 3, 4 };
+		for ()
+			System.out.println("For = "+ num.length );
+				
+		int array = { 2 *=};
+		for(number : array) {
+			
+		}
+	    if (num %=2 ) {
+	}continue{
+		System.out.println("Continue" + )
 	}
 }
