45eed8a3fd24d5cdf878405b5545effe8fc633e9
chihirotanaka <61111335+chihirotanaka@users.noreply.github.com>
2020-08-01 20:46:25 +0900

    [fix]9

---
+++ JavaWcp/src/practice/Car.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class Car {
+
+	private double fuelCost;
+
+	Private double fuelAmount;
+
+//	コンストラクタを作成
+
+//	moveメソッド
+
+//	."@@ km 走ります"を出力
+	System.out.println(  + "km走ります");
+//	.残量を計算
+
+//	fuelAmountを取得するメソッドを作成
+
+}
