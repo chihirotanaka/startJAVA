e653ee574e38685164d2f97a20aca0db88fe6d72
chihirotanaka <61111335+chihirotanaka@users.noreply.github.com>
2020-08-04 03:45:11 +0900

    [practice]9

---
+++ JavaWcp/src/practice4.java
@@ -0,0 +1,14 @@
+import practice.Car;
+
+public class practice4 {
+	public static void main(String[] args) {
+		double fuelCost = 12.5;
+
+		double fuelAmount = 55.0;
+
+		Car car = new Car(fuelCost, fuelAmount);
+
+		car.move(20);
+		System.out.println("残量は" + car.getFuelAmount() +"です。");
+	}
+}
