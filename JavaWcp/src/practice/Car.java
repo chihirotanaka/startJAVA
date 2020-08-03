80d476e7777e7853f06ee051aac6434bd6b3d2d9
chihirotanaka <61111335+chihirotanaka@users.noreply.github.com>
2020-08-03 16:49:36 +0900

    [finish]9

--- JavaWcp/src/practice/Car.java
+++ JavaWcp/src/practice/Car.java
@@ -7,13 +7,22 @@ public class Car {
 	Private double fuelAmount;
 
 //	コンストラクタを作成
+	public Car(double fuelCost, double fuelAmount) {
+		this.fuelCost = fuelCost;
+		this.fuelCost = fuelAmount;
+	}
 
 //	moveメソッド
 
 //	."@@ km 走ります"を出力
-	System.out.println(  + "km走ります");
+	public void move(int km) {
+		System.out.println(km + "km走ります");
+		this.fuelAmount -= (km / fuelcost);
+	}
 //	.残量を計算
 
 //	fuelAmountを取得するメソッドを作成
-
+	public double getFuelAmount() {
+		return this.fuelAmount;
+	}
 }
